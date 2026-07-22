c2d96e77255530c292eade67e9b419e74d845ab4
Jhomar Artes <jhomarartes444@gmail.com>
2026-07-21 22:11:45 -0500

    Configurar app para Render

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request, jsonify
 
 from utils import predecir
@@ -23,4 +25,11 @@ def prediccion():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
